90f7f559e0c96fc698c5c9bb90bc18b3020efc95
RC Cola <41647118+Royal-Crown-Cola@users.noreply.github.com>
2019-06-05 20:30:02 -0600

    deleted shit

--- server.js
+++ server.js
@@ -3,7 +3,20 @@ var path = require("path");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
-
+var waitingList = [];
+var reservationList = [{
+        1: "Ryan"
+    },
+    {
+        2: "Stetson"
+    },
+    {
+        3: "Matthew"
+    },
+    {
+        4: "Bryan"
+    }
+];
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({
     extended: true
@@ -23,31 +36,33 @@ app.get("/view", function (req, res) {
     res.sendFile(path.join(__dirname, "reservation_view.html"));
 });
 
-app.get("/view/reservations", function(req, res) {
+app.get("/view/reservations", function (req, res) {
     return res.json(reservationList);
-  });
+});
 
-  app.get("/view/waitList", function(req, res) {
+app.get("/view/waitList", function (req, res) {
     return res.json(waitingList);
-  });
+});
 
-  app.post("/reservation", function(req, res) {
+app.post("/reservation", function (req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
     var newParty = req.body;
-  
+
     // Using a RegEx Pattern to remove spaces from newCharacter
     // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
     newParty.routeName = newParty.name.replace(/\s+/g, "").toLowerCase();
-  
-    console.log(newParty);
-  
-    reservationList.push(newParty);
-  
-    res.json(newParty);
-  });
 
-app.listen(PORT, function() {
+    if (reservationlist.length >= 5) {
+        waitingList.push(newReservation);
+    } else {
+        reservationList.push(newParty);
+    }
+    console.log(newParty);
+
+    res.json(newParty);
+});
+
+app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
-  });
-  
+});
